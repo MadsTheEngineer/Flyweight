5bbf7504f78d743412d5dc18e3b4f7714011210f
Jonas Hitomi Balslev Ishibori <20013575@post.au.dk>
2014-12-02 17:22:36 +0100

    Example program

---
+++ FlyweightExample/ObjectFactory/PixelFactory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelFactory
+{
+    public enum PixelColor
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    public class Rectangle
+    {
+        public Bitmap Picture { get; set; }//intrinsic
+        public int X { get; set; }//extrinsic
+        public int Y { get; set; }//extrinsic
+
+        public Rectangle(Bitmap b,int x, int y)
+        {
+            Picture = b;
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class PixelFactory
+    {
+        public int ObjectsCount = 0;
+        private Dictionary<PixelColor, Bitmap> _pixelColors;
+
+        public void Reset()
+        {
+            _pixelColors.Clear();
+        }
+
+        public Bitmap GetBitmap(PixelColor instanceType)
+        {
+            if (_pixelColors == null)
+                _pixelColors = new Dictionary<PixelColor, Bitmap>();
+
+            if (_pixelColors.ContainsKey(instanceType))
+                return _pixelColors[instanceType];
+
+            switch (instanceType)
+            {
+                case PixelColor.Red:
+                    {
+                        _pixelColors.Add(instanceType, new Bitmap("../../images/red.bmp"));
+                        ObjectsCount++;
+                        break;
+                    }
+
+                case PixelColor.Green:
+                    {
+                        _pixelColors.Add(instanceType, new Bitmap("../../images/green.bmp"));
+                        ObjectsCount++;
+                        break;
+                    }
+
+                case PixelColor.Blue:
+                    {
+                        _pixelColors.Add(instanceType, new Bitmap("../../images/blue.bmp"));
+                        ObjectsCount++;
+                        break;
+                    }
+            }
+
+            return _pixelColors[instanceType];
+        }
+
+    }
+
+}
---
+++ FlyweightExample/ObjectFactory/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PixelFactory;
+using Rectangle = PixelFactory.Rectangle;
+
+namespace ObjectFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pixelListFlyweight = new List<Rectangle>();
+            var pixelListNormal = new List<Rectangle>();
+            
+            var myPixelFactory = new PixelFactory.PixelFactory();
+
+            var rnd = new Random();
+            var s = new Stopwatch();
+
+            
+                s.Start();
+                for (int i = 0; i < 260; i++)
+                {
+                    //var bitmap = myPixelFactory.GetBitmap((PixelColor) rnd.Next(3));
+                    //pixelListFlyweight.Add(new Rectangle(bitmap, rnd.Next(100), rnd.Next(100)));
+                    pixelListNormal.Add(new Rectangle(new Bitmap("../../images/red.bmp"), rnd.Next(100), rnd.Next(100)));
+                }
+                s.Stop();
+                //pixelListFlyweight.Clear();
+                //pixelListNormal.Clear();
+                
+                //myPixelFactory.Reset();
+                Console.WriteLine(s.Elapsed.Milliseconds);
+                s.Reset();
+            
+            
+            //Console.WriteLine(myPixelFactory.ObjectsCount);
+            Console.ReadKey();
+        }
+    }
+}
